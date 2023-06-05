1fe3b357552112f7b7652a248c8f1ff4b013bce6
Javier Miranda <Jabox2002@proton.me>
2023-06-04 23:17:28 -0300

    Proyecto

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
-// Carousel
-const buttons = document.querySelectorAll('[data-carousel-button]');
+// Mostrar lista del carrito
+const cartIcon = document.querySelector('#cart-icon');
+const closeCart = document.querySelector('.close__cart');
+const objetivo = document.querySelector('.container__cart-products');
 
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        const offset = button.dataset.carouselButton === 'next' ? 1 : -1;
-        const slides = button.closest('[data-carousel]').querySelector('[data-slides]');
-
-        const activeSlide = slides.querySelector("[data-active]");
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-        if (newIndex < 0) newIndex = slides.children.length - 1;
-        if (newIndex >= slides.children.length) newIndex = 0;
-
-        slides.children[newIndex].dataset.active = true;
-        delete activeSlide.dataset.active;
-    });
+cartIcon.addEventListener('click', () => {
+    objetivo.classList.toggle('container__cart-products--active');
 });
+closeCart.addEventListener('click', () => {
+    objetivo.classList.toggle('container__cart-products--active');
+});
+
+
+
+
+// // Carrito
+// const carrito = document.querySelector('#carrito');
+// const listaCursos = document.querySelector('#lista-cursos');
